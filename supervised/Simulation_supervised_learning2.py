48be64981c827a999b66162b64105f754f89cbf1
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2025-03-13 21:13:56 +0900

    Update Simulation_supervised_learning2.py

--- supervised/Simulation_supervised_learning2.py
+++ supervised/Simulation_supervised_learning2.py
@@ -377,6 +377,6 @@ if __name__=="__main__":
     ASR_1=Heuristic(grid_size=(5,5),TP_type_len=3,mod='ASR')
     Random_1=Heuristic(grid_size=(5,5),TP_type_len=3,mod='Random')
     BLF_1=Heuristic(grid_size=(5,5),TP_type_len=3,mod='BLF')
-    ppo=PPO(feature_dim=4, hidden_dim=32, lookahead_block_num=1,grid_size=(5,5), learning_rate=0.001, lmbda=0.95, gamma=1, alpha=0.5, beta=0.01, epsilon=0.2, mod='GCN2').to(device)
+    ppo=PPO(feature_dim=4, hidden_dim=32, lookahead_block_num=1,grid_size=(5,5), learning_rate=0.001, lmbda=0.95, gamma=1, alpha=0.5, beta=0.01, epsilon=0.2, mod='GCN1').to(device)
     #ST_sim.Train(train_step=1000,eval_step=1,K=500,pr_num=500,batch_num=1,simulation_day=10,lookahead_num=1,ppo=ppo,model_dir='',ASR_1=ASR_1,Random_1=Random_1,BLF_1=BLF_1)
     ST_sim.Train(train_data_num=1000, update_num=10000, train_step_num=1000, eval_step=10, pr_num=20, batch_num=50, K=5, simulation_day=10, lookahead_num=1,ppo=ppo,model_dir=model_dir,ASR_1=ASR_1,Random_1=Random_1,BLF_1=BLF_1,history_dir=history_dir)
