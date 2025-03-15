da25d1a69219e1bd8d6346158de20b3637dd8c08
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2025-03-15 15:09:38 +0900

    Update Simulation_supervised_learning2.py

--- supervised/Simulation_supervised_learning2.py
+++ supervised/Simulation_supervised_learning2.py
@@ -269,7 +269,7 @@ class Stockyard_simulation:
             eval_set.append([grid.copy(),total_block.copy(),total_block_encoded.copy()])
 
         ave_rearrangement=0
-        '''
+        
         for ev_set in eval_set:
             for _____ in range(5):
                 total_rearrangement,grids,blocks,actions,rewards,dones,masks,probs,block_lefts=self.Run_simulation(simulation_day,lookahead_num,ASR_1,ev_set[0].copy(),ev_set[1].copy(),ev_set[2].copy())
@@ -287,7 +287,6 @@ class Stockyard_simulation:
                 total_rearrangement,grids,blocks,actions,rewards,dones,masks,probs,block_lefts=self.Run_simulation(simulation_day,lookahead_num,BLF_1,ev_set[0].copy(),ev_set[1].copy(),ev_set[2].copy())
                 ave_rearrangement+=total_rearrangement
         print('BLF',ave_rearrangement/pr_num/batch_num)
-        '''
         gridss = []
         blockss = []
         actionss = []
