cda09835398ac8760fa44050f8668666cc8e73b4
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2025-03-16 22:05:48 +0900

    Update Simulation_vessl.py

--- supervised/Simulation_vessl.py
+++ supervised/Simulation_vessl.py
@@ -367,11 +367,14 @@ if __name__=="__main__":
     if not os.path.exists(input_dir):
         os.makedirs(input_dir)
     device='cuda'
-    ST_sim=Stockyard_simulation(yard_size=(5,5),initial_block=5,lam=1/250,weight=(100,501),TP_type=[200,350,550],Block_per_Day=(5,7),mod=0)
-    ASR_1=Heuristic(grid_size=(5,5),TP_type_len=3,mod='ASR')
-    Random_1=Heuristic(grid_size=(5,5),TP_type_len=3,mod='Random')
-    BLF_1=Heuristic(grid_size=(5,5),TP_type_len=3,mod='BLF')
-    ppo=PPO(feature_dim=4, hidden_dim=32, lookahead_block_num=1,grid_size=(5,5), learning_rate=0.001, lmbda=0.95, gamma=1, alpha=0.5, beta=0.01, epsilon=0.2, mod='GCN2').to(device)
+    pr_size=(7,7)
+    init_block=10
+    bpd=(10,14)
+    ST_sim=Stockyard_simulation(yard_size=pr_size,initial_block=init_block,lam=1/250,weight=(100,501),TP_type=[200,350,550],Block_per_Day=bpd,mod=0)
+    ASR_1=Heuristic(grid_size=pr_size,TP_type_len=3,mod='ASR')
+    Random_1=Heuristic(grid_size=pr_size,TP_type_len=3,mod='Random')
+    BLF_1=Heuristic(grid_size=pr_size,TP_type_len=3,mod='BLF')
+    ppo=PPO(feature_dim=4, hidden_dim=32, lookahead_block_num=1,grid_size=pr_size, learning_rate=0.001, lmbda=0.95, gamma=1, alpha=0.5, beta=0.01, epsilon=0.2, mod='GCN2').to(device)
     checkpoint = torch.load(input_dir+'DGCN_supervised.pth',map_location=torch.device('cuda'))  # 파일에서 로드할 경우
     full_state_dict = checkpoint['model_state_dict']
 
