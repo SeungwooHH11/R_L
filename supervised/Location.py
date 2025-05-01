18b6751e32a9b8b56113ed0fc0e5a660aae309bc
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2025-05-01 09:17:22 +0900

    Update Location.py

--- supervised/Location.py
+++ supervised/Location.py
@@ -432,13 +432,13 @@ class PPO(nn.Module):
         advantage_lst = np.zeros((b,1))
         advantage_lst = torch.tensor(advantage_lst, dtype=torch.float32).to(device)
         i = 0
-        for ep in ep_len:
-            advantage = 0.0
-            
-            for t in reversed(range(i, i + ep)):
-                advantage = self.gamma * self.lmbda * advantage + delta[t][0]
-                advantage_lst[t][0] = advantage
-            i += ep    
+        for e,ep in enumerate(ep_len):
+            if e<10:
+                advantage = 0.0
+                for t in reversed(range(i, i + ep)):
+                    advantage = self.gamma * self.lmbda * advantage + delta[t][0]
+                    advantage_lst[t][0] = advantage
+                i += ep    
         eps = 1e-8 
         ratio = torch.exp(torch.log(pi_cal) - torch.log(probs+eps))  # a/b == exp(log(a)-log(b))
 
