f41c70d9488af6a02eb473fa79f088ba43b018e2
SeoungwooHan <150769291+SeungwooHH11@users.noreply.github.com>
2025-05-05 01:30:57 +0900

    Update Simulation_vessl.py

--- supervised/Simulation_vessl.py
+++ supervised/Simulation_vessl.py
@@ -388,6 +388,6 @@ if __name__=="__main__":
     # 필터링된 가중치만 로드
     ppo.load_state_dict(filtered_state_dict, strict=False)
     '''
-    history=ST_sim.Train(train_step=1500,eval_step=20,K=2,pr_num=7,batch_num=20,simulation_day=10,lookahead_num=1,ppo=ppo,model_dir=model_dir,ASR_1=ASR_1,Random_1=Random_1,BLF_1=BLF_1)
+    history=ST_sim.Train(train_step=1000,eval_step=20,K=2,pr_num=5,batch_num=15,simulation_day=10,lookahead_num=1,ppo=ppo,model_dir=model_dir,ASR_1=ASR_1,Random_1=Random_1,BLF_1=BLF_1)
     history=pd.DataFrame(history)
     history.to_excel('history.xlsx', sheet_name='Sheet', index=False)
